822830d08c75b00ff41b8798e7f3af07d9647dbb
Dev <devman@gmail.com>
2018-10-19 19:25:03 -0300

    added more info in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo "\n\n Installing depedences...\n\n"
+
 pip install youtube-dl
 pip install pafy
 
 composer install
 
+echo "\n\n updating feed...\n\n"
+
+
 php update.php
+
+echo "\n\nPlease add to cron php update.php to automatically check updates\n\n"
