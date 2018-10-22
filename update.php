a94dbd8bd6e7057785eee38eaba80fa495ac6e64
devgyn <devgyn@users.noreply.github.com>
2018-10-21 22:58:48 -0300

    starting download mode and fix spaces

--- update.php
+++ update.php
@@ -1,7 +1,20 @@
 <?php
 
+/**
+ * APP Configuration
+ *
+ * mode:
+ *      default: use default, the links is by google
+ *      download: download the podcasts into downloads folder and use local links
+ */
+$appConfig = [
+    'mode' => 'default'
+];
+
+
+
 if (PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) {
-	throw new Exception('CLI only');
+    throw new Exception('CLI only');
 }
 
 require_once 'vendor/autoload.php';
@@ -10,28 +23,28 @@ require_once 'vendor/autoload.php';
 $ytChannelIDs = json_decode(file_get_contents('channels.json'));
 
 if (!isset($ytChannelIDs->channels)) {
-	throw new Exception('Invalid channels items');
+    throw new Exception('Invalid channels items');
 }
 
 $urls = null;
 
 foreach ($ytChannelIDs->channels as $ytch)
 {
-	// get videos by youtube feed url
-	$youtubeFeed = file_get_contents('https://www.youtube.com/feeds/videos.xml?channel_id=' . $ytch);
+    // get videos by youtube feed url
+    $youtubeFeed = file_get_contents('https://www.youtube.com/feeds/videos.xml?channel_id=' . $ytch);
 
-	if (!$youtubeFeed) {
-		throw new Exception('Youtube feed unreachable');
-	}
+    if (!$youtubeFeed) {
+        throw new Exception('Youtube feed unreachable');
+    }
 
-	$ytXML = new SimpleXMLElement($youtubeFeed);
+    $ytXML = new SimpleXMLElement($youtubeFeed);
 
 
-	foreach ($ytXML->entry as $entry)
-	{
-		$ytvID = str_replace('yt:video:', '', $entry->id);
-		$urls[] = 'https://www.youtube.com/watch?v=' . $ytvID;
-	}
+    foreach ($ytXML->entry as $entry)
+    {
+        $ytvID = str_replace('yt:video:', '', $entry->id);
+        $urls[] = 'https://www.youtube.com/watch?v=' . $ytvID;
+    }
 
 }
 
@@ -41,32 +54,38 @@ $feed = new \Suin\RSSWriter\Feed();
 $channel = new \Suin\RSSWriter\Channel();
 
 $channel
-	->title('Youtube to podcast audio')
-	->description('fetch youtube audios')
-	->appendTo($feed);
+    ->title('Youtube to podcast audio')
+    ->description('fetch youtube audios')
+    ->appendTo($feed);
 
 if (!$urls) {
-	// if urls is empty, do nothing
-	exit;
+    // if urls is empty, do nothing
+    exit;
 }
 
 foreach ($urls as $url)
 {
-	$exec = json_decode(exec('python ./getAudioLink.py ' . $url));
+    $exec = null;
 
-	if (!isset($exec->title) || !isset($exec->url)) {
-		continue;
-	}
+    if ($appConfig['mode'] == 'default') {
+        $exec = json_decode(exec('python ./getAudioLink.py ' . $url));
 
-	$item = new \Suin\RSSWriter\Item();
+        if (!isset($exec->title) || !isset($exec->url)) {
+            continue;
+        }
+    }
 
-	$item
-	->title($exec->title)
-	->description($exec->author)
-	->url($url)
-	->enclosure($exec->url)
-	->pubDate(strtotime($exec->date))
-	->appendTo($channel);
+    if ($exec) {
+        $item = new \Suin\RSSWriter\Item();
+
+        $item
+        ->title($exec->title)
+        ->description($exec->author)
+        ->url($url)
+        ->enclosure($exec->url)
+        ->pubDate(strtotime($exec->date))
+        ->appendTo($channel);
+    }
 }
 
 file_put_contents('feed.xml', $feed);
